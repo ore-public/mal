d95ca5d5515b886b829125e1d1c4399d4fe42218
Masaya Konishi <konishi@office-ore.net>
2018-10-09 19:21:21 +0900

    step1 途中2

--- ruby2/reader.rb
+++ ruby2/reader.rb
@@ -17,12 +17,40 @@ class Reader
   end
 end
 
+def tokenizer(str)
+  re = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/
+  str.scan(re).flatten
+end
+
+def read_atom(reader)
+
+end
+
+def read_list(reader)
+  list = Array.new
+  c = reader.next
+  if c == '('
+    raise "list start not '('"
+  end
+
+  while true
+    c = read_form(reader)
+    list.push(c)
+    break if c == ')'
+  end
+end
+
+def read_form(reader)
+  case reader.peek
+  when '('
+    read_list(reader)
+  else
+    read_atom(reader)
+  end
+end
+
 def read_str(str)
   tokens = tokenizer(str)
   read_form(Reader.new(tokens))
 end
 
-def tokenizer(str)
-  re = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/
-  str.scan(re).flatten
-end
