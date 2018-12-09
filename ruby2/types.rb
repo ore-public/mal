bea9b1e2105ae00c23ce18aefb733b864cecde4a
Masaya Konishi <konishi@office-ore.net>
2018-12-09 11:23:57 +0900

    step5

--- ruby2/types.rb
+++ ruby2/types.rb
@@ -16,3 +16,19 @@ class Dictionary < Array
     Hash[self.each_slice(2).to_a]
   end
 end
+
+class Function
+  attr_accessor :ast
+  attr_accessor :env
+  attr_accessor :params
+
+  def initialize(ast=nil, env=nil, params=nil)
+    self.ast = ast
+    self.env = env
+    self.params = params
+  end
+
+  def get_env(args)
+    return Env.new(self.env, self.params, args)
+  end
+end
